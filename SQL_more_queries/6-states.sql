c2627aeb8355e806be5553b3ad4d7faab2e4fba6
chykB <malikchika86@gmail.com>
2023-08-15 14:43:38 -0700

    create the database hbtn_0d_usa and the table states

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- a script that creates the database hbtn_0d_usa and the table states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
-	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY_KEY,
+	id INT AUTO_INCREMENT PRIMARY_KEY NOT NULL,
 	name VARCHAR(256) not null
 	);
